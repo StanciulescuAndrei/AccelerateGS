d12b1de9404c67608ca64264bc9ca1e069b3ef0a
Andrei <stanciulescu.andrei@yahoo.com>
2024-06-06 16:06:46 +0900

    god bless DAAL

--- src/PointClustering.h
+++ src/PointClustering.h
@@ -1,20 +1,36 @@
 #ifndef __POINT_CLUSTERING__
 #define __POINT_CLUSTERING__
 
-#include "oneapi/dal/algo/dbscan.hpp"
-#include "oneapi/dal/io/csv.hpp"
+#include "daal.h"
 
-namespace dal = oneapi::dal;
+namespace dm = daal::data_management;
+namespace algo =  daal::algorithms;
 
 void DBSCANClustering(){
-    const auto x_data = dal::read<dal::table>(dal::csv::data_source{ nullptr });
+    float data[8] = {0.0f, 1.0f, 0.0f, 1.001f, 2.0f, 0.0f, 2.001f, 0.0f};
 
-    double epsilon = 0.04;
-    std::int64_t min_observations = 45;
-    auto dbscan_desc = dal::dbscan::descriptor<>(epsilon, min_observations);
-    dbscan_desc.set_result_options(dal::dbscan::result_options::responses);
+    dm::NumericTablePtr pointData = dm::HomogenNumericTable<>::create(data, 2, 4);
 
-    const auto result_compute = dal::compute(dbscan_desc, x_data);
+    daal::algorithms::dbscan::Batch<> dbscanClusterer(1e-3, 2);
+    dbscanClusterer.input.set(daal::algorithms::dbscan::data, pointData);
+
+    dbscanClusterer.compute();
+
+    dm::NumericTablePtr result = dbscanClusterer.getResult()->get(algo::dbscan::assignments);
+
+    size_t nRows = result->getNumberOfRows();
+    size_t nCols = result->getNumberOfColumns();
+
+    dm::BlockDescriptor<int> block;
+    result->getBlockOfRows(0, nRows, dm::ReadWriteMode::readOnly, block);
+    int *array = block.getBlockPtr();
+    printf("%d, %d\n", nRows, nCols);
+    for(int i=0;i<nRows;i++){
+        printf("-> %d\n", array[i]);
+    }
+    printf("----------------\n");
+    result->releaseBlockOfRows(block);
+
 }
 
 #endif
